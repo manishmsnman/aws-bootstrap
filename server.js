808842e9bd134ab79578c938333de25eab850e0f
MMD <manish.gauranga@gmail.com>
2021-07-05 23:39:11 +0530

    Changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { hostname } = require("os");
 const http = require("http");
 
 const message = "Hello World\n";
-const PORT = 3002;
+const PORT = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
